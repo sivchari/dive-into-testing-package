1f10e942a8c1be58f658fbbbfa56bdb35f0fd08e
sivchari <shibuuuu5@gmail.com>
2022-10-13 01:39:16 +0900

    finish

--- example/testing/trunner.go
+++ example/testing/trunner.go
@@ -1,5 +1,7 @@
 package testing
 
+import "time"
+
 func tRunner() {
 	// START OMIT
 	tRunner(t, func(t *T) {
@@ -8,4 +10,33 @@ func tRunner() {
 		}
 	})
 	// END OMIT
+}
+
+func tRunner2() {
+	// START2 OMIT
+	if len(t.sub) > 0 {
+		// Run parallel subtests.
+		// Decrease the running count for this test.
+		t.context.release()
+		// Release the parallel subtests.
+		close(t.barrier)
+		// Wait for subtests to complete.
+		for _, sub := range t.sub {
+			<-sub.signal
+		}
+		cleanupStart := time.Now()
+		err := t.runCleanup(recoverAndReturnPanic)
+		t.duration += time.Since(cleanupStart)
+		if err != nil {
+			doPanic(err)
+		}
+		if !t.isParallel {
+			// Reacquire the count for sequential tests. See comment in Run.
+			t.context.waitParallel()
+		}
+	} else if t.isParallel {
+		// Only release the count for this test if it was run as a parallel test. See comment in Run method.
+		t.context.release()
+	}
+	// END2 OMIT
 }
